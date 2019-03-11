340a529881a4b6fd905753be21b3caf6ea7719f7
meigaromamaro <meigarom.lopes@amaro.com>
2019-03-11 09:50:50 -0300

    add pearson coefficient

--- linearRegression_lesson01.R
+++ linearRegression_lesson01.R
@@ -1,12 +1,27 @@
-# --------------------------------------
-# Lessons 1.4 - students height x weight
-# --------------------------------------
-# read dataset
-dataset = read.csv( '../dataset/student_height_weight.csv', stringsAsFactor=F )
+## --------------------------------------
+## Lessons 1.4 - students height x weight
+## --------------------------------------
+## read dataset
+#dataset = read.csv( '../dataset/student_height_weight.csv', stringsAsFactor=F )
+#
+## training model
+#lr_model = lm( wt ~ ht, data=dataset )
+#
+## print analysis
+#print( summary( lr_model ) )
+#print( anova( lr_model ) )
 
-# training model
-lr_model = lm( wt ~ ht, data=dataset )
+# --------------------------------------
+# Lessons 1.5 - skin cancer
+# --------------------------------------
+dataset = read.csv( 'dataset/skincancer.csv', stringsAsFactor=F )
+lr_model = lm( Mort ~ Lat, data=dataset )
 
-# print analysis
+print( '===> Pearson Correlation' )
+print( cor( dataset$Lat, dataset$Mort, method='pearson' ) )
+
+print( '===> Model summary' )
 print( summary( lr_model ) )
+
+print( '===> Analysis of Variance' )
 print( anova( lr_model ) )
